98a69fc60c91303d49f29bd967e69b9976a8eea2
alanliveira <alanliveira@outlook.com>
2020-12-05 11:35:17 -0300

    Adicionado todo o progresso da aula

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,77 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # GET /posts/new
+  def new
+    @post = current_user.posts.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+    p current_user
+    @post = current_user.posts.find(params[:id])
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = current_user.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: "Post criado com sucesso 🎉🎉🎉." }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "Post alterado com sucesso 🎉🎉🎉." }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to posts_url, notice: "Post deletado com sucesso." }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:content, :like)
+  end
+end
